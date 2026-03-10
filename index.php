f7b5b1b9387d99210567b73e631c4de9a388c4ad
Widyalifya <widyazahra102@gmail.com>
2026-03-10 22:40:33 +0700

    update tugas

--- index.php
+++ index.php
@@ -4,20 +4,16 @@ include "Mahasiswa.php";
 
 $mahasiswaTI = new Mahasiswa();
 $mahasiswaTI->setData('A11.2000.00001', 'Fulan', 'Teknik Informatika'); 
-print_r($mahasiswaTI->getData());
+
 
 
 $mahasiswaSI = new Mahasiswa();
 $mahasiswaSI->setData('A12.2000.00001', 'Junior', 'Sistem Informasi');
-print_r($mahasiswaSI->getData());
-echo "<br><br>";
 
+echo "<br><br>";
 
 $mahasiswaBaru = new Mahasiswa();
 $mahasiswaBaru->setData('A11.2023.12345', 'Budi', 'Teknik Informatika');
-print_r($mahasiswaBaru->getData());
-echo "<hr>";
-
 
 echo "<b>Data Mahasiswa:</b><br><br>";
 $mahasiswaTI->tampilkanData();
--- lat2/Mahasiswa.php
+++ lat2/Mahasiswa.php
@@ -3,6 +3,22 @@ class Mahasiswa
 {
     private $nim,$matkul;
     public $nama;
+    
+public function setData($nim, $nama, $matkul)
+    {
+        $this->nim = $nim;
+        $this->nama = $nama;
+        $this->matkul = $matkul;
+    } 
+    public function getData()
+    {
+        return [
+            $this->nim,
+            $this->nama,
+            $this->matkul,
+        ];
+
+    }
 
 }
 
